d27f00f4d3bb8ee7c11e74e761f0db5ba79209fa
jgmchan <jgmchan@gmail.com>
2014-09-08 23:00:41 +1000

    Add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Install the dotfiles to their rightful place
+for file in gitconfig slate tmux.conf vimrc zshrc; do
+  ln -f -s $PWD/$file ~/.$file
+done
+
+ln -f -s $PWD/oh-my-zsh/themes/jeff.zsh-theme ~/.oh-my-zsh/themes/jeff.zsh-theme
